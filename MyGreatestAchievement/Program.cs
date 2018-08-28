1b93fad8a8ffbd91947c2334a6589674c56a11dc
Chase Aucoin <chase@cupcakefactory,net>
2018-08-27 21:19:11 -0500

    Initial Commit

---
+++ MyGreatestAchievement/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace MyGreatestAchievement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Beep(1047, 300);
+                Console.Beep(1047, 300);
+                Console.Beep(932, 300);
+                Console.Beep(1047, 300);
+                Thread.Sleep(300);
+                Console.Beep(784, 400);
+                Console.Beep(784, 400);
+                Console.Beep(1047, 300);
+                Console.Beep(1397, 300);
+                Console.Beep(1319, 300);
+                Console.Beep(1047, 300);
+                Thread.Sleep(800);
+                Console.Beep(784, 300);
+                Console.Beep(784, 300);
+                Console.Beep(698, 300);
+                Console.Beep(784, 300);
+                Thread.Sleep(300);
+                Console.Beep(587, 400);
+                Console.Beep(587, 400);
+                Console.Beep(784, 300);
+                Console.Beep(1047, 300);
+                Console.Beep(988, 300);
+                Console.Beep(784, 300);
+                Thread.Sleep(800);
+            }
+        }
+    }
+}
